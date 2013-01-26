81da8cc60b9d53a9e332af72785872e59cf75c45
Adam Wardell <awardell@ucsc.edu>
2013-01-26 12:42:45 -0800

    DeathCondition.cs - Heart game object destroyed on stopped moving

--- Assets/Code/DeathCondition.cs
+++ Assets/Code/DeathCondition.cs
@@ -3,15 +3,34 @@ using System.Collections;
 
 public class DeathCondition : MonoBehaviour {
 
-    Heart heart;
+    private Heart heart;
+    private Rigidbody rigidbody;
+    private Transform transform;
+
+    public float deathTriggerVelocity;
+    public float deathTriggerHeight;
 
 	// Use this for initialization
 	void Start () {
         heart = (Heart)GetComponent<Heart>();
+        rigidbody = (Rigidbody)GetComponent<Rigidbody>();
+        transform = (Transform)GetComponent<Transform>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+        Debug.Log("isThrown: " + heart.isThrown);
+	    if (heart.isThrown &&
+            rigidbody.velocity.magnitude < deathTriggerVelocity &&
+            transform.position.y < deathTriggerHeight)
+        {
+            BeginDeath();
+        }
 	}
+
+    void BeginDeath()
+    {
+        Debug.Log("YOU'VE DIED. GOOD JOB.");
+        Object.Destroy(gameObject);
+    }
 }
