bc4877b583c760144ae7e608accaaf7b0df4ae54
Adam Wardell <awardell@ucsc.edu>
2013-01-26 22:50:57 -0800

    Buncha stuff with the menus

---
+++ Assets/HowToPlay.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+using System.Collections;
+
+public class HowToPlay : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+    void LoadHTPScene()
+    {
+        Application.LoadLevel("TutorialMenu");
+    }
+}
